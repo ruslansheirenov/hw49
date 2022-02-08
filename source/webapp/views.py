77ed8791c4031335853318d6438269a4d8aae8cb
sheirenov <ruslansheirenov@gmail.com>
2022-02-09 02:53:12 +0600

    Созданы классовое представление IndexView, базовый шаблон и шаблон отображения всех задач

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import View, TemplateView
+
+from .models import Issue
 
 # Create your views here.
+
+class IndexView(TemplateView):
+    def get(self, request, **kwargs):
+        issues = Issue.objects.all()
+        return render(request, 'index.html', {'issues': issues})
+
