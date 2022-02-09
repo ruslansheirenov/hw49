32b9c19dc7a412ef85bfac82e88bb43a37eea6e4
sheirenov <ruslansheirenov@gmail.com>
2022-02-10 00:17:33 +0600

    Переписана форма на модельную форму

--- source/webapp/forms.py
+++ source/webapp/forms.py
@@ -1,11 +1,9 @@
 from django import forms
-from django.forms import widgets
 
-from .models import TypeModel, StatusModel
+from .models import Issue
 
 
 class IssueForm(forms.Form):
-    summary = forms.CharField(max_length=100, required=True, label='Summary')
-    description = forms.CharField(max_length=2000, required=True, label='description', widget=widgets.Textarea)
-    type = forms.ModelChoiceField(queryset=TypeModel.objects.all())
-    status = forms.ModelChoiceField(queryset=StatusModel.objects.all())
+    class Meta:
+        model = Issue
+        exlude = []
