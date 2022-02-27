c1183d531d4593e587a6e497a8ce2feaeb97c79b
sheirenov <ruslansheirenov@gmail.com>
2022-02-27 14:47:23 +0600

    Добалена регистрация, созданы шаблон, представление и форма для регистрации

--- source/accounts/urls.py
+++ source/accounts/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 
-from accounts.views import login_view, logout_view
+from accounts.views import login_view, logout_view, RegisterView
 
 
 app_name = 'accounts'
@@ -10,4 +10,5 @@ app_name = 'accounts'
 urlpatterns = [
     path('accounts/login/', LoginView.as_view(), name='login'),
     path('accounts/logout/', LogoutView.as_view(), name='logout'),
+    path('accounts/create/', RegisterView.as_view(), name='register'),
 ]
--- source/accounts/views.py
+++ source/accounts/views.py
@@ -1,7 +1,33 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.views.generic import CreateView
+from django.contrib.auth.models import User
+
+from .forms import MyUserCreationForm
 
 # Create your views here.
+
+#Регистрация
+
+class RegisterView(CreateView):
+    model = User
+    template_name = 'user_create.html'
+    form_class = MyUserCreationForm
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect(self.get_success_url())
+
+    def get_success_url(self):
+        next_url = self.request.GET.get('next')
+        if not next_url:
+            next_url = self.request.POST.get('next')
+        if not next_url:
+            next_url = reverse('index')
+        return next_url
+
+#Вход
 
 def login_view(request):
     context = {}
@@ -16,6 +42,7 @@ def login_view(request):
             context['has_error'] = True
     return render(request, 'login.html', context=context)
 
+#Выход
 
 def logout_view(request):
     logout(request)
